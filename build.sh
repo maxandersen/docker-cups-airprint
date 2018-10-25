09cd6ef9d0eca6df677a19d694c0c52d98ef176d
Max Rydahl Andersen <manderse@redhat.com>
2018-10-25 12:27:12 +0200

    make it not nuke existing services

---
+++ build.sh
@@ -0,0 +1,1 @@
+sudo docker build -t maxandersen/cups-airprint .
--- root/root/run_cups.sh
+++ root/root/run_cups.sh
@@ -9,7 +9,7 @@ echo $CUPSADMIN:$CUPSPASSWORD | chpasswd
 
 mkdir -p /config/ppd
 mkdir -p /services
-rm -rf /etc/avahi/services/*
+#rm -rf /etc/avahi/services/*
 rm -rf /etc/cups/ppd
 ln -s /config/ppd /etc/cups
 if [ `ls -l /services/*.service 2>/dev/null | wc -l` -gt 0 ]; then
@@ -20,6 +20,6 @@ if [ `ls -l /config/printers.conf 2>/dev/null | wc -l` -eq 0 ]; then
 fi
 cp /config/printers.conf /etc/cups/printers.conf
 
-/usr/sbin/avahi-daemon --daemonize
+#/usr/sbin/avahi-daemon --daemonize
 /root/printer-update.sh &
 exec /usr/sbin/cupsd -f
